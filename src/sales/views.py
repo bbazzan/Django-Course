06e255447f2b94de33bdd612aa345b2896f6b7cd
brunobazzan <brunobazzan@gmail.com>
2022-01-27 15:11:47 -0300

    created detail list and made made it posible to navigate to it & created search form

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 
 # Create your views here.
 
 
 def home_view(request):
+    form = SalesSearchForm(request.POST or None)
     hello = 'hello world from the view'
-    return render(request, 'sales/home.html', {'hello': hello})
+    context = {
+        'hello': hello,
+        'form': form
+    }
+    return render(request, 'sales/home.html', context)
 
 
 class SaleListView(ListView):
     model = Sale
     template_name = 'sales/main.html'
+
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = 'sales/detail.html'
+
+
+# def sale_list_view(request):
+#     qs = Sale.objects.all()
+#     return render(request, 'sales/main.html', {'object_list': qs})
+#
+#
+# def sale_detail_view(request, **kwargs):
+#     pk = kwargs.get('pk')
+#     obj = Sale.objects.get(pk=pk)
+#     return render(request, 'sales/detail.html', {'object': obj})
